2ac6fcc0659be9e345e39c7e9cf5aef5cf891f56
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-15 17:34:15 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+def my_collect(students)
+  student = []
+  i = 0
+  while i < students.length
+ student.push yield(students[i])
+  i +=1
+  end
+  student
+end
 
 
+
+my_collect(students) { |student| student.split(" ").first}

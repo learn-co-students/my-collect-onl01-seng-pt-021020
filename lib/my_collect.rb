559193b0ccb0a7a44ac84f993022dcda5ccf2d26
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-16 09:52:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+def my_collect(array)
+  i = 0
+  result = []
+  while i < array.length
+    
+    result << ( yield(array[i]) )
+    puts ( yield(array[i]) )
+    i += 1
+  end
+  array = result
+  array
+end
 
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+
+
+#["Tim", "Tom", "Jim"]

b4ba98ec18beec15944be5c94f3b19f0f7dd34bb
Tim Donoghue <tmd112593@gmail.com>
2020-02-23 20:55:11 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(collection)
+  new_collection = []
+  index = 0 
+  while index < collection.length do 
+    new_collection << yield(collection[index])
+    index += 1 
+  end
+  new_collection
+end 
+  
 
